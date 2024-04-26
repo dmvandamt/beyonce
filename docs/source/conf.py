afb7bef9a7e1fffe85fdd32f31e80c4f58a74193
dmvandam <dmvandam@deloitte.nl>
2024-04-26 08:59:18 +0200

    fixed conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages'
 ]
 
 intersphinx_mapping = {
@@ -53,13 +55,15 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
